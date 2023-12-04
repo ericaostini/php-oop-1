b937436f36cfa8b370d786cc8e5a74cf190df687
ericaostini <ericaostini14@gmail.com>
2023-12-04 16:38:52 +0100

    genre list creation foreach type of genre

--- Model/Genres.php
+++ Model/Genres.php
@@ -11,7 +11,13 @@ class Genres
 
 $genreString = file_get_contents(__DIR__ . "/genre_db.json");
 $genreArray = json_decode($genreString, true);
-var_dump($genreArray);
+// var_dump($genreArray);
+
+$genreList = [];
+foreach ($genreArray as $class) {
+    $genreList[] = new Genres($class);
+}
+var_dump($genreList);
 
 //prova funzionamento function construct
 // $nuovoGenere = new Genres("Series");
