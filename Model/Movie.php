1bd41d5f201698e17b798baea12bdd6baca12700
ericaostini <ericaostini14@gmail.com>
2023-12-04 15:21:13 +0100

    create constructor function

--- Model/Movie.php
+++ Model/Movie.php
@@ -7,6 +7,16 @@ class Movie
     public $poster_path;
     public $vote_average;
     public $original_language;
+
+    function __construct($_id, $_title, $_overview, $_image, $_vote, $_language)
+    {
+        $this->id = $_id;
+        $this->title = $_title;
+        $this->overview = $_overview;
+        $this->poster_path = $_image;
+        $this->vote_average = $_vote;
+        $this->original_language = $_language;
+    }
 }
 
 
