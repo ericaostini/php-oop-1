3fee7568c3f2c1ae0f94468d3ebe22f9429c021a
ericaostini <ericaostini14@gmail.com>
2023-12-04 16:36:14 +0100

    read data json file for genres

--- Model/Genres.php
+++ Model/Genres.php
@@ -9,6 +9,10 @@ class Genres
     }
 }
 
+$genreString = file_get_contents(__DIR__ . "/genre_db.json");
+$genreArray = json_decode($genreString, true);
+var_dump($genreArray);
+
 //prova funzionamento function construct
 // $nuovoGenere = new Genres("Series");
 // var_dump($nuovoGenere)
